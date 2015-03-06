c436a66d1258a284b423a12c3684092d4694fdb9
Daniel Kang <dgk001@ucsd.edu>
2015-03-06 05:06:34 -0800

    minor modificatino to filter. now allows lookup of parts of the title as well.

--- routes/takeAwalk.js
+++ routes/takeAwalk.js
@@ -11,7 +11,6 @@ exports.takeAwalk = function(req, res) {
 
 exports.filterEvent = function(req,res){
 	var searchValue = req.body.selectInput;
-	
 	var index = 0;
 	//create a new json object
 	var JSONObj = {
@@ -29,11 +28,14 @@ exports.filterEvent = function(req,res){
 		
 		}
 		else{
+			var lowerCaseTitle = (data.takeAwalk[i].title).toLowerCase();
 			//This will search the the desciption and pick out words that match
 			var split = data.takeAwalk[i].description.split(" ");
+			var splitTitle = lowerCaseTitle.split(" ");
 			for(var j = 0; j < (split.length); j++){
-				console.log(split[j]);
-				if(searchValue === split[j]){
+				//console.log(split[j]);
+				console.log(splitTitle[j]);
+				if(searchValue === split[j] || searchValue === splitTitle[j]){
 					var item2 = data.takeAwalk[i];
 					JSONObj.takeAwalk.push(item2);
 				}
